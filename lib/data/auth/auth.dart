e10ad3f7a1fdbf132779019a1c548b432dc9ba98
Artem Marakhovskyi <artem.marakhovskyi@gmail.com>
2025-02-17 17:01:27 +0100

    add firebase

--- lib/data/auth/auth.dart
+++ lib/data/auth/auth.dart
@@ -1,11 +1,1 @@
-import 'package:firebase_auth/firebase_auth.dart';
 
-class Auth {
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-  User? get currentUser => _firebaseAuth.currentUser;
-
-
-  Future<void> signInWithGoogle({
-    
-  })
-}
